0582895ea55e411adf843657c65a25c1e8345e0d
Claudia Garcia <claudia.garcia21@estudiant.upf.edu>
2024-10-20 21:56:01 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -4,6 +4,36 @@
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	import sys
+
+	n = int(sys.argv[1])
+
+	def comptar_divisors(n):
+		comptador = 0
+		divisor = 1
+		while n >= divisor:
+			if n % divisor == 0:
+					comptador +=1
+			divisor +=1
+		return comptador
+
+	def main():
+		n = int(sys.argv[1])
+
+		divisors_n = comptar_divisors(n)
+
+		es_anitprime = True
+		i = 1
+		while i < n:
+			if comptar_divisors(i) >= divisors_n:
+				es_anitprime = False
+				break
+			i += 1
+		if es_anitprime:
+			print("antiprime")
+		else:
+			print("not anti-prime")
+
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
